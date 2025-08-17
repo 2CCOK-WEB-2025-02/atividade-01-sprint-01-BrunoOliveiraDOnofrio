d84406cd60d98d2f0a19be595ea70a1fff9f4fea
BrunoOliveiraDOnofrio <bruno.donofrio@sptech.school>
2025-08-16 23:57:12 -0300

    Finalização dos exercícios

--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilController.java
@@ -4,11 +4,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.Array;
+
 @RestController
 public class ExercicioDificilController {
 
     @GetMapping("/ex-03/{n}")
-    public ExercicioDificilResponse exercicioDificil(int n) {
-        return null;
+    public ExercicioDificilResponse exercicioDificil(@PathVariable int n) {
+        if (n <= 0) {
+            return new ExercicioDificilResponse(0,0);
+        }
+        int primeiro = 0;
+        int segundo = 1;
+
+        int[] veto = new int [n + 1];
+
+        int somador = 0;
+
+        for (int i = 0; i <= n; i++) {
+            veto[i] = primeiro;
+            int repassa = primeiro;
+            primeiro = segundo;
+            segundo = repassa + segundo;
+        }
+
+        for (int i = 0; i <= n; i++) {
+            somador += veto[i];
+        }
+
+        return new ExercicioDificilResponse(veto[n], somador);
     }
 }
--- src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
+++ src/main/java/school/sptech/sprint1_nota1/ex3/ExercicioDificilResponse.java
@@ -1,5 +1,31 @@
 package school.sptech.sprint1_nota1.ex3;
 
 public class ExercicioDificilResponse {
+    //Atributo
+    private int enesimoTermo;
+    private int soma;
+    //Construtor
+    //CHEIO
+    public ExercicioDificilResponse(int enesimoTermo, int soma) {
+        this.enesimoTermo = enesimoTermo;
+        this.soma = soma;
+    }
+    //VAZIO
+    public ExercicioDificilResponse(){}
+    //Método
+    public int getEnesimoTermo() {
+        return enesimoTermo;
+    }
 
+    public void setEnesimoTermo(int enesimoTermo) {
+        this.enesimoTermo = enesimoTermo;
+    }
+
+    public int getSoma() {
+        return soma;
+    }
+
+    public void setSoma(int soma) {
+        this.soma = soma;
+    }
 }
